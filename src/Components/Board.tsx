97107687d28d2bb1ddf77faf77e2428571da92c6
seorang42 <seorang42@naver.com>
2023-07-11 19:26:31 +0900

    7.10 Cross Board Movement

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -29,7 +29,11 @@ function Board({ toDos, boardId }: IBoardProps) {
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
         {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
+          <div
+            style={{ backgroundColor: "red" }}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
             {toDos.map((toDo, index) => (
               <DraggableCard key={toDo} index={index} toDo={toDo} />
             ))}
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -10,5 +10,6 @@ export const toDoState = atom<IToDoState>({
     "To Do": ["a", "b"],
     Doing: ["c", "d"],
     Done: ["e", "f"],
+    "Do Later": ["g", "h"],
   },
 });
